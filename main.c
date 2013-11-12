c99152ec4d08b0490ff493f3401b958ccaa26fe5
Marco Rossini <marco.rossini@phys.ethz.ch>
2013-11-12 10:22:50 +0100

    Using the new ADC_init() function from the separate file.

--- main.c
+++ main.c
@@ -7,32 +7,13 @@
 
 #include "usart.h"
 #include "timer.h"
+#include "adc.h"
 
 int main (void) {
 	USART_init();
 	timer_8bit_cnt0_init();
 	timer_16bit_cnt1_init();
-
-	/* ### Set up the ADC */
-
-	/* Use AVCC (5 V) as voltage reference */
-	ADMUX |= (1 << REFS0);
-	ADMUX &= ~(1 << REFS1);
-
-	/* Set ADC input to differential mode with inputs channel 0 (+)
-	   and channel 1 (-) and gain 10*/
-	ADMUX &= ~(0x1f);
-	ADMUX |= (1 << MUX3);
-	ADMUX |= (1 << MUX0);
-
-	/* Left align the 10 bit ADC value in the 16 bit register */
-	ADMUX |= (1 << ADLAR);
-
-	/* Enable the ADC */
-	ADCSRA |= (1 << ADEN);
-
-	/* Set ADC clock prescaler to 1/128 */
-	ADCSRA |= (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2);
+	ADC_init();
 
 	/* The temperature setpoint (in ADC units) */
 	int16_t setpoint = 0;
