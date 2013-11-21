5efa79cccc91f903b90b689341bf5491a25b21c5
Marco Rossini <marco.rossini@phys.ethz.ch>
2013-11-21 12:24:42 +0100

    Fixed the EEPROM addresses (offset between 16 bit integers is 2).

--- eeprom.h
+++ eeprom.h
@@ -5,12 +5,12 @@
 #include <avr/eeprom.h>
 
 /* Define the addresses of values in the EEPROM memory */
-#define EE_CTRL_SETPOINT	((uint16_t *) 0x1)
-#define EE_CTRL_KP0		((uint16_t *) 0x2)
-#define EE_CTRL_KI0		((uint16_t *) 0x3)
-#define EE_CTRL_KD0		((uint16_t *) 0x4)
-#define EE_CTRL_KP1		((uint16_t *) 0x5)
-#define EE_CTRL_KI1		((uint16_t *) 0x6)
-#define EE_CTRL_KD1		((uint16_t *) 0x7)
+#define EE_CTRL_SETPOINT	((uint16_t *) 0x2)
+#define EE_CTRL_KP0		((uint16_t *) 0x4)
+#define EE_CTRL_KI0		((uint16_t *) 0x6)
+#define EE_CTRL_KD0		((uint16_t *) 0x8)
+#define EE_CTRL_KP1		((uint16_t *) 0xa)
+#define EE_CTRL_KI1		((uint16_t *) 0xc)
+#define EE_CTRL_KD1		((uint16_t *) 0xe)
 
 #endif /* __EEPROM_H__ */
